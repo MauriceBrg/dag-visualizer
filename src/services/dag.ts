8d799c9bcca9dbddecc28771a87bdc2e5e9ef908
Maurice Borgmeier <mborgmeier@tecracer.de>
2023-01-17 15:37:37 +0100

    Try to fix path to json document

--- src/services/dag.ts
+++ src/services/dag.ts
@@ -2,7 +2,7 @@ import { DagElement } from "../entities/dag-element";
 
 export async function getDag(): Promise<DagElement[]> {
 
-    const response = await fetch("dag-visualizer/sample_dag.json")
+    const response = await fetch(process.env.PUBLIC_URL + "/sample_dag.json")
     const data = await response.json()
 
     return data
